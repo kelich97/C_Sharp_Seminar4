81279c8a7fdeb897eba5b44a6a127887992bb6ec
Kamalov Keldibek <klbest1797@gmail.com>
2023-06-26 17:31:25 +0300

    Exercise 29

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int [] NewArray (int size_array)
+{
+    int[] array = new int[size_array];
+    for(int i = 0; i < size_array; i++)
+    {
+        array[i] = new Random().Next(0, 500);
+    }
+    return array;
+}
+
+void PrintArr (int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.WriteLine("Input size");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] remember = NewArray(size);
+PrintArr(remember);
+
+
