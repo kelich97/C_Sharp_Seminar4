19f4a82371bf16148c70bc3c8ffbe85039a64325
kelich97 <135359551+kelich97@users.noreply.github.com>
2023-06-26 17:39:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-// 6, 1, 33 -> [6, 1, 33]
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
 
-
-int [] NewArray (int size_array)
+int Sum(int num)
 {
-    int[] array = new int[size_array];
-    for(int i = 0; i < size_array; i++)
-    {
-        array[i] = new Random().Next(0, 500);
+    
+    int result = 0;
+    while(num > 0)
+    {    
+        result = result + num % 10;
+        num = num / 10;
     }
-    return array;
+    return result;   
 }
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(number);
+Console.WriteLine(sum);
 
-void PrintArr (int[]array)
-{
-    for(int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-}
 
-Console.WriteLine("Input size");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] remember = NewArray(size);
-PrintArr(remember);
+
+
+
+
+
 
 
